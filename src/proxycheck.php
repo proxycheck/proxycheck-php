a1637219e2594eaad6b2229605a63dd2d9d3002b
proxycheck.io <support@proxycheck.io>
2019-05-18 06:08:34 +0100

    Added stats export support
    Improved country handling, now supports allowing specific countries to be white listed essentially.
    Added support for the viewing of all stats from our export endpoint.
    Improved error handling for when some parameters are not provided (such as countries).

--- src/proxycheck.php
+++ src/proxycheck.php
@@ -12,8 +12,8 @@
       // Setup the correct querying string for the transport security selected.
       if ( $Options['TLS_SECURITY'] == TRUE ) { $URL = "https://"; } else { $URL = "http://"; }
       
-      // Check if they have enabled the blocked country feature by providing countries.
-      if ( !empty($Options['BLOCKED_COUNTRIES']) ) {
+      // Check if they have enabled blocking or allowing countries and if so, enable ASN checking.
+      if ( !empty($Options['BLOCKED_COUNTRIES']) OR !empty($Options['ALLOWED_COUNTRIES'])) {
         $Options['ASN_DATA'] = 1;
       }
       
@@ -55,7 +55,7 @@
       // Decode the JSON from our API
       $Decoded_JSON = json_decode($API_JSON_Result, true);
 
-      // Check if the IP we're testing is a proxy server
+      // Output the clear block and block reasons for the IP we're checking.
       if ( isset($Decoded_JSON[$Visitor_IP]["proxy"]) && $Decoded_JSON[$Visitor_IP]["proxy"] == "yes" && $Decoded_JSON[$Visitor_IP]["type"] == "VPN" ) {
         
         $Decoded_JSON["block"] = "yes";
@@ -66,16 +66,24 @@
         $Decoded_JSON["block"] = "yes";
         $Decoded_JSON["block_reason"] = "proxy";
         
-      } else if ( isset($Decoded_JSON[$Visitor_IP]["proxy"]) && in_array($Decoded_JSON[$Visitor_IP]["country"],$Options['BLOCKED_COUNTRIES']) ) {
-        
-        $Decoded_JSON["block"] = "yes";
-        $Decoded_JSON["block_reason"] = "country";
-        
       } else {
         
         $Decoded_JSON["block"] = "no";
         $Decoded_JSON["block_reason"] = "na";
         
+      }
+      
+      // Country checking for blocking and allowing specific countries by name or isocode.
+      if ( $Decoded_JSON["block"] == "no" && !empty($Options['BLOCKED_COUNTRIES']) ) {
+          if ( in_array($Decoded_JSON[$Visitor_IP]["country"],$Options['BLOCKED_COUNTRIES']) OR in_array($Decoded_JSON[$Visitor_IP]["isocode"],$Options['BLOCKED_COUNTRIES'])  ) {
+            $Decoded_JSON["block"] = "yes";
+            $Decoded_JSON["block_reason"] = "country";
+          }
+      } else if ( $Decoded_JSON["block"] == "yes" && !empty($Options['ALLOWED_COUNTRIES'])) {
+          if ( in_array($Decoded_JSON[$Visitor_IP]["country"],$Options['ALLOWED_COUNTRIES']) OR in_array($Decoded_JSON[$Visitor_IP]["isocode"],$Options['ALLOWED_COUNTRIES'])  ) {
+            $Decoded_JSON["block"] = "no";
+            $Decoded_JSON["block_reason"] = "na";
+          }
       }
       
       return $Decoded_JSON;
@@ -87,7 +95,7 @@
       // Setup the correct querying string for the transport security selected.
       if ( $Options['TLS_SECURITY'] == TRUE ) { $URL = "https://"; } else { $URL = "http://"; }
       
-      // Build up the URL string with the selected flags.
+      // Build up the URL string for the selected list and action.
       $URL .= "proxycheck.io/dashboard/" . $Options['LIST_SELECTION'] . "/" . $Options['LIST_ACTION'] . "/";
       $URL .= "?key=" . $Options['API_KEY'];
       
@@ -108,6 +116,45 @@
         CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_POST => 1,
         CURLOPT_POSTFIELDS => $Post_Field,
+        CURLOPT_RETURNTRANSFER => true
+      );
+      
+      curl_setopt_array($ch, $curl_options);
+      $API_JSON_Result = curl_exec($ch);
+      curl_close($ch);
+      
+      // Decode the JSON from our API
+      $Decoded_JSON = json_decode($API_JSON_Result, true);
+      
+      return $Decoded_JSON;
+      
+    }
+
+    public static function stats($Options) {
+
+      // Setup the correct querying string for the transport security selected.
+      if ( $Options['TLS_SECURITY'] == TRUE ) { $URL = "https://"; } else { $URL = "http://"; }
+      
+      // Build up the URL string for the selected export stat.
+      $URL .= "proxycheck.io/dashboard/export/" . $Options['STAT_SELECTION'] . "/";
+      $URL .= "?key=" . $Options['API_KEY'];
+      
+      if ( strcasecmp($Options['STAT_SELECTION'], "detections") == 0 OR strcasecmp($Options['STAT_SELECTION'], "queries") == 0  ) {
+        $URL .= "&json=1";
+      }
+      
+      if ( strcasecmp($Options['STAT_SELECTION'], "detections") == 0 ) {
+        if ( empty($Options['LIMIT']) ) { $Options['LIMIT'] = 100; }
+        if ( empty($Options['OFFSET']) ) { $Options['OFFSET'] = 0; }
+        $URL .= "&limit=" . $Options['LIMIT'];
+        $URL .= "&offset=" . $Options['OFFSET'];
+      }
+      
+      // Performing the API query to proxycheck.io/dashboard/ using cURL
+      $ch = curl_init($URL);
+      
+      $curl_options = array(
+        CURLOPT_CONNECTTIMEOUT => 30,
         CURLOPT_RETURNTRANSFER => true
       );
       
